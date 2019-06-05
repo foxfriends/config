64024737901806f61b2c5081b9cf148502065d22
Cameron Eldridge <cameldridge+git@gmail.com>
2019-06-05 02:03:07 -0400

    Adjust some things

--- config.fish
+++ config.fish
@@ -1,38 +1,40 @@
-#!/usr/local/bin/fish
+#!fish
 
-if status is-login
-  # attach autojump
-  if test -f "/usr/local/Cellar/autojump/22.5.3/share/autojump/autojump.fish"
-    . /usr/local/Cellar/autojump/22.5.3/share/autojump/autojump.fish
-  end
+# attach autojump
+if test -f "/usr/local/Cellar/autojump/22.5.3/share/autojump/autojump.fish"
+  . /usr/local/Cellar/autojump/22.5.3/share/autojump/autojump.fish
+end
 
-  if test -f "$HOME/.asdf/asdf.fish"
-    . "$HOME/.asdf/asdf.fish"
-  end
-  
-  alias icat="kitty +kitten icat"
-  alias vi=nvim
-  alias top=htop
-  alias tree="git log --graph --oneline"
+if test -f "/usr/share/autojump/autojump.fish"
+  . "/usr/share/autojump/autojump.fish"
+end
 
-  # enable colors in grep by default
-  alias grep='grep --color=auto'
-  alias fgrep='fgrep --color=auto'
-  alias egrep='egrep --color=auto'
+if test -f "$HOME/.asdf/asdf.fish"
+  . "$HOME/.asdf/asdf.fish"
+end
 
-  # replace ls with exa
-  alias ls='exa'
-  alias ll='exa -alg --git'
-  alias lt='exa -T'
-  alias llt='exa -lT'
-  alias l='exa'
+alias icat="kitty +kitten icat"
+alias vi=nvim
+alias top=htop
+alias tree="git log --graph --oneline"
 
-  # Some typos I often make
-  alias clearls="clear; ls"
-  alias sgit="git"
+# enable colors in grep by default
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
-  # Local stuff can be put in ~/.config,fish
-  if test -f "$HOME/.config.fish" 
-    . "$HOME/.config.fish"
-  end
+# replace ls with exa
+alias ls='exa'
+alias ll='exa -alg --git'
+alias lt='exa -T'
+alias llt='exa -lT'
+alias l='exa'
+
+# Some typos I often make
+alias clearls="clear; ls"
+alias sgit="git"
+
+# Local stuff can be put in ~/.config,fish
+if test -f "$HOME/.config.fish" 
+  . "$HOME/.config.fish"
 end
