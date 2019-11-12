dfdb57f00b130a0c4d11eca007b7584fa1ba2835
Cameron Eldridge <cameldridge@gmail.com>
2019-11-11 19:45:51 -0500

    use pazi instead of autojump, improvement to git branch in prompt

--- config.fish
+++ config.fish
@@ -1,12 +1,7 @@
 #!fish
 
-# attach autojump
-if test -f "/usr/local/Cellar/autojump/22.5.3/share/autojump/autojump.fish"
-  . /usr/local/Cellar/autojump/22.5.3/share/autojump/autojump.fish
-end
-
-if test -f "/usr/share/autojump/autojump.fish"
-  . "/usr/share/autojump/autojump.fish"
+if command -v pazi >/dev/null
+  status --is-interactive; and pazi init fish | source
 end
 
 alias icat="kitty +kitten icat"
@@ -38,4 +33,24 @@ end
 
 if test -x /usr/bin/lesspipe
   eval (env SHELL=/bin/sh lesspipe)
+end
+
+if test "$TERM" = "linux"
+  echo -en "\e]P0282C34" #black
+  echo -en "\e]P83E4452" #darkgrey
+  echo -en "\e]P1E06C75" #darkred
+  echo -en "\e]P9BE5046" #red
+  echo -en "\e]P298C378" #darkgreen
+  echo -en "\e]PA98C379" #green
+  echo -en "\e]P3E5C07B" #brown
+  echo -en "\e]PBD19A66" #yellow
+  echo -en "\e]P461AFEF" #darkblue
+  echo -en "\e]PC61AFEF" #blue
+  echo -en "\e]P5C678DD" #darkmagenta
+  echo -en "\e]PDC678DD" #magenta
+  echo -en "\e]P656B6C2" #darkcyan
+  echo -en "\e]PE56B6C2" #cyan
+  echo -en "\e]P7ABB2BF" #lightgrey
+  echo -en "\e]PF5C6370" #white
+  clear #for background artifacting
 end
