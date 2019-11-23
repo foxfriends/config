f8756389c126c5954d1c8b4b00e5a36a110290b6
Cameron Eldridge <cameldridge@gmail.com>
2019-11-23 16:30:56 -0500

    Add syncat to reskin

--- functions/reskin.fish
+++ functions/reskin.fish
@@ -3,4 +3,5 @@ function reskin --description 'Change the colour schemes of all the things that 
     
     kitty @ set-colors -a -c ~/.config/kitty/colors/$skin.conf
     source ~/.config/fish/colors/$skin.fish
+    set -gx syncat_active_style $skin
 end
