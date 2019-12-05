6de43ef2cf2b1d1044e8fa54910cf32791ef67ff
Cameron Eldridge <cameldridge@gmail.com>
2019-12-04 21:21:30 -0500

    Add panda

--- functions/reskin.fish
+++ functions/reskin.fish
@@ -1,7 +1,10 @@
 function reskin --description 'Change the colour schemes of all the things that need to be changed'
     set -Ux skin $argv[1]
-    
-    kitty @ set-colors -a -c ~/.config/kitty/colors/$skin.conf
+
+    if test "$TERM" = "xterm-kitty"
+        kitty @ set-colors -a -c ~/.config/kitty/colors/$skin.conf
+    end
+
     source ~/.config/fish/colors/$skin.fish
     set -gx syncat_active_style $skin
 end
