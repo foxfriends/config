2419fa09d02de30220353a2d8cedb7aa0bff68a2
Cameron Eldridge <cameldridge@gmail.com>
2020-11-12 20:01:12 -0500

    Dont print errors when reskinning first time

--- functions/reskin.fish
+++ functions/reskin.fish
@@ -9,12 +9,12 @@ function reskin --description 'Change the colour schemes of all the things that 
     set -gx syncat_active_style $skin
 
     if test -L "$HOME/.gitconfig"
-        unlink "$HOME/.gitconfig"
+        unlink "$HOME/.gitconfig" 2> /dev/null
         ln -s "$HOME/.config/git/.gitconfig.$skin" "$HOME/.gitconfig"
     end
 
     if test -d "$HOME/.config/broot/"
-        rm "$HOME/.config/broot/conf.toml" "$HOME/.config/broot/conf-kak.toml"
+        rm -f "$HOME/.config/broot/conf.toml" "$HOME/.config/broot/conf-kak.toml"
         cat "$HOME/.config/broot/conf.base.toml" "$HOME/.config/broot/colors.$skin.toml" > "$HOME/.config/broot/conf.toml"
         cat "$HOME/.config/broot/conf-kak.base.toml" "$HOME/.config/broot/colors.$skin.toml" > "$HOME/.config/broot/conf-kak.toml"
     end
