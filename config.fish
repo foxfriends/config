1e215d381897821435624b794b11eeff810a2735
Cameron Eldridge <cameldridge@gmail.com>
2020-12-18 19:43:42 -0500

    No more hologram

--- config.fish
+++ config.fish
@@ -65,3 +65,5 @@ end
 if test -x /usr/bin/lesspipe
   eval (env SHELL=/bin/sh lesspipe)
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
--- functions/reskin.fish
+++ functions/reskin.fish
@@ -9,11 +9,11 @@ function reskin --description 'Change the colour schemes of all the things that 
     source ~/.config/fish/colors/$skin.fish
     set -gx syncat_active_style $skin
 
-    if test -L "$HOME/.gitconfig" -a ! -p "$HOME/.gitconfig"
-        cat "$HOME/.config/git/.gitconfig" "$HOME/.config/git/.gitconfig.$skin" > "$HOME/.config/git/.gitconfig.current"
+    if test -L "$HOME/.gitconfig"
+        cat "$HOME/.config/git/.gitconfig.$skin" > "$HOME/.config/git/.gitconfig.current"
     end
 
-    if test -d "$HOME/.config/broot/" -a ! -p "$HOME/.config/broot/conf.toml"
+    if test -d "$HOME/.config/broot/"
         rm -f "$HOME/.config/broot/conf.toml" "$HOME/.config/broot/conf-kak.toml"
         cat "$HOME/.config/broot/conf.base.toml" "$HOME/.config/broot/colors.$skin.toml" > "$HOME/.config/broot/conf.toml"
         cat "$HOME/.config/broot/conf-kak.base.toml" "$HOME/.config/broot/colors.$skin.toml" > "$HOME/.config/broot/conf-kak.toml"
