db85db1cac20e8e6903a65e8986a6c29c3c4f5ad
Cameron Eldridge <cameldridge+git@gmail.com>
2021-11-14 23:46:09 -0500

    Use command -sq instead of command -q

--- config.fish
+++ config.fish
@@ -3,30 +3,30 @@
 
 set -q skin; or set -Ux skin onedark
 
-if command -q ghcup
+if command -sq ghcup
   set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 end
 
 if status --is-interactive
   reskin $skin
-  command -q pazi; and source (pazi init fish |psub)
-  command -q rbenv; and source (rbenv init -|psub)
-  command -q pyenv; and source (pyenv init -|psub)
-  command -q kitty; and source (kitty + complete setup fish |psub)
-  command -q diesel; and source (diesel completions fish |psub)
-  command -q deno; and source (deno completions fish |psub)
-  command -q rustup; and source (rustup completions fish |psub)
-  command -q gh; and source (gh completion -s fish |psub)
-  if command -q fnm
+  command -sq pazi; and source (pazi init fish |psub)
+  command -sq rbenv; and source (rbenv init -|psub)
+  command -sq pyenv; and source (pyenv init -|psub)
+  command -sq kitty; and source (kitty + complete setup fish |psub)
+  command -sq diesel; and source (diesel completions fish |psub)
+  command -sq deno; and source (deno completions fish |psub)
+  command -sq rustup; and source (rustup completions fish |psub)
+  command -sq gh; and source (gh completion -s fish |psub)
+  if command -sq fnm
     source (fnm completions |psub)
     source (fnm env |psub)
   end
 
-  command -q aws; and alias localaws="command aws --endpoint-url=http://localhost:4566 --profile localstack"
-  command -q aws aws-mfa-secure; and alias aws="aws-mfa-secure session"
+  command -sq aws; and alias localaws="command aws --endpoint-url=http://localhost:4566 --profile localstack"
+  command -sq aws aws-mfa-secure; and alias aws="aws-mfa-secure session"
 
   # replace ls with exa
-  if command -q exa >/dev/null
+  if command -sq exa >/dev/null
     alias ls='exa'
     alias ll='exa -alg --git'
     alias lt='exa -T'
@@ -39,7 +39,7 @@ if status --is-interactive
   alias fgrep='fgrep --color=auto'
   alias egrep='egrep --color=auto'
 
-  command -q lesspipe; and eval (env SHELL=/bin/sh lesspipe)
+  command -sq lesspipe; and eval (env SHELL=/bin/sh lesspipe)
 end
 
 # Local stuff can be put in ~/.config.fish
