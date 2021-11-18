d9c75ff6025b924587cf804fb51716f53ea6cfba
Cameron Eldridge <cameldridge@gmail.com>
2021-11-18 14:35:28 -0500

    Remove git cg

--- functions/git.fish
+++
@@ -1,7 +0,0 @@
-function c --description 'git fuzzy-checkout'
-  if test $argv[1] = '-'
-    git checkout -
-  else
-    git branch | sed 's/^\*//' | string trim | fzf -1 -0 -q "$argv" | xargs git checkout
-  end
-end
